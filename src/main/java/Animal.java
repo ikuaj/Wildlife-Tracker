7297a1ba7caa95281374ff91356028fb4a0c0e9a
james ikua <ikuaj96@gmail.com>
2018-05-28 20:01:28 +0300

    vtl files and app.java

---
+++ src/main/java/Animal.java
@@ -0,0 +1,99 @@
+import org.sql2o.*;
+import java.util.List;
+
+public class Animal{
+    private String endangered;
+    private String animal;
+    private String health;
+    private String age;
+    private int id;
+    
+    public Animal(String animal, String endangered, String health, String age){
+        this.animal = animal;
+        this.endangered = endangered;
+        this.health = health;
+        this.age = age;
+    }
+
+    public String getEndangered(){
+        return endangered;
+    }
+
+    public String getAnimal(){
+        return animal;
+    }
+    public String getHealth(){
+        return health;
+    }
+    public String age(){
+        return age;
+    }
+    public int getId(){
+        return id;
+    }
+
+    // public void save(){
+    //     try(Connection con = DB.sql2o.open()){
+    //         String sql = "INSERT INTO animals(animal, endangered, health, age) VALUES(:animal, :endangered, :health, :age)";
+    //         this.id = (int) con.createQuery(sql, true)
+    //         .addParameter("animal", this.animal)
+    //         .addParameter("endangered", this.endangered)
+    //         .addParameter("health", this.health)
+    //         .addParameter("age", this.age)
+    //         .throwOnMappingFailure(false)
+    //         .executeUpdate()
+    //         .getKey();
+
+    //     }
+    // }
+    
+    // public static List<Animal> all() {
+    //     String sql = "select * from animals";
+    //     try(Connection con = DB.sql2o.open()) {
+    //        return con.createQuery(sql)
+    //        .throwOnMappingFailure(false)
+    //        .executeAndFetch(Animal.class);
+    //     }
+    // }
+
+    public static String getAnimalName(int id) {
+        String sql = "select animal from animals where id = :id;";
+        try(Connection con = DB.sql2o.open()) {
+           String name = con.createQuery(sql)
+           .addParameter("id", id)
+           .executeScalar(String.class);
+           return name;
+        }
+    }
+
+    public static String getAnimalEndangered(int id) {
+        String sql = "select endangered from animals where id = :id;";
+        try(Connection con = DB.sql2o.open()) {
+           String endangered = con.createQuery(sql)
+           .addParameter("id", id)
+           .executeScalar(String.class);
+           return endangered;
+        }
+    }
+
+    public static String getAnimalHealth(int id) {
+        String sql = "select health from animals where id = :id;";
+        try(Connection con = DB.sql2o.open()) {
+           String health = con.createQuery(sql)
+           .addParameter("id", id)
+           .executeScalar(String.class);
+           return health;
+        }
+    }
+
+    public static String getAnimalAge(int id) {
+        String sql = "select age from animals where id = :id;";
+        try(Connection con = DB.sql2o.open()) {
+           String age = con.createQuery(sql)
+           .addParameter("id", id)
+           .executeScalar(String.class);
+           return age;
+        }
+    }
+}
+
---
+++ src/main/java/Sighting.java
@@ -0,0 +1,51 @@
+// import org.sql2o.*;
+// import java.util.List;
+// import java.sql.Timestamp;
+
+// public class Sighting{
+//     private String ranger;
+//     private String location;
+//     private int animal_id;
+//     private Timestamp timestamp;
+//     private int id;
+
+//     public Sighting (String ranger, String location, int animal_id){
+//         this.ranger = ranger;
+//         this.location = location;
+//         this.animal_id = animal_id;
+//     }
+
+//     public String getRanger(){
+//         return ranger;
+//     }
+//     public String getLocation(){
+//         return location;
+//     }
+//     public int getAnimalId(){
+//         return animal_id;
+//     }
+//     public Timestamp getTimestamp(){
+//         return timestamp;
+//     }
+//     public int getId(){
+//         return id;
+//     }
+//     public void save() {
+//         try(Connection con = DB.sql2o.open()) {
+//            String sql = "INSERT INTO sightings (ranger, location, animal_id, timestamp) VALUES (:ranger, :location, :animal_id, now())";
+//            this.id = (int) con.createQuery(sql, true)
+//            .addParameter("ranger", this.ranger)
+//            .addParameter("location", this.location)
+//            .addParameter("animal_id", this.animal_id)
+//            .executeUpdate()
+//            .getKey();
+//         }
+//      }
+//      public static List<Sighting> all() {
+//         String sql = "select * from sightings";
+//         try(Connection con = DB.sql2o.open()) {
+//            return con.createQuery(sql)
+//            .executeAndFetch(Sighting.class);
+//         }
+//      }
+// }
