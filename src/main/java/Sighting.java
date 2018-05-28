b125825d632fe5bbb1ab04649973e91573bc7063
james ikua <ikuaj96@gmail.com>
2018-05-28 20:03:43 +0300

    sighting file

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -1,35 +1,35 @@
-// import org.sql2o.*;
-// import java.util.List;
-// import java.sql.Timestamp;
+import org.sql2o.*;
+import java.util.List;
+import java.sql.Timestamp;
 
-// public class Sighting{
-//     private String ranger;
-//     private String location;
-//     private int animal_id;
-//     private Timestamp timestamp;
-//     private int id;
+public class Sighting{
+    private String ranger;
+    private String location;
+    private int animal_id;
+    private Timestamp timestamp;
+    private int id;
 
-//     public Sighting (String ranger, String location, int animal_id){
-//         this.ranger = ranger;
-//         this.location = location;
-//         this.animal_id = animal_id;
-//     }
+    public Sighting (String ranger, String location, int animal_id){
+        this.ranger = ranger;
+        this.location = location;
+        this.animal_id = animal_id;
+    }
 
-//     public String getRanger(){
-//         return ranger;
-//     }
-//     public String getLocation(){
-//         return location;
-//     }
-//     public int getAnimalId(){
-//         return animal_id;
-//     }
-//     public Timestamp getTimestamp(){
-//         return timestamp;
-//     }
-//     public int getId(){
-//         return id;
-//     }
+    public String getRanger(){
+        return ranger;
+    }
+    public String getLocation(){
+        return location;
+    }
+    public int getAnimalId(){
+        return animal_id;
+    }
+    public Timestamp getTimestamp(){
+        return timestamp;
+    }
+    public int getId(){
+        return id;
+    }
 //     public void save() {
 //         try(Connection con = DB.sql2o.open()) {
 //            String sql = "INSERT INTO sightings (ranger, location, animal_id, timestamp) VALUES (:ranger, :location, :animal_id, now())";
